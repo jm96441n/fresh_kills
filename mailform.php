5bdc32c16c830679cb8021b15b0aff634472df09
John Maguire <jmaguire5588@gmail.com>
2016-06-24 12:30:24 -0400

    first commit

---
+++ mailform.php
@@ -0,0 +1,17 @@
+<?php
+$to      = 'john@johnmaguiredeveloper.com';
+$subject = 'New Request from Website';
+$message = $_POST['mytextarea'];
+$headers = 'From: freshkillsbar.com' . "\r\n" .
+'Reply-To: john@johnmaguiredeveloper.com' . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+$send = mail($to, $subject, $message, $headers);
+
+// this will help you to get the status mail sent or not
+if($send) :
+   echo "Email sent";
+else :
+    echo "Email sending failed";
+endif;
+?>
