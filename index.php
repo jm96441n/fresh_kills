6b502492519d988243236f329e7fbafb02fac58a
John Maguire <jmaguire5588@gmail.com>
2016-06-25 11:12:35 -0400

    trying to fix email form

--- index.php
+++ index.php
@@ -85,10 +85,10 @@
 			$email = $_POST['email'];
 			$message = $_POST['message'];
 			$formcontent="From: $name \n Message: $message";
-			$recipient = "john@johnmaguiredeveloper.com";
+			// $recipient = "john@johnmaguiredeveloper.com";
 			$subject = "Contact Form";
 			$mailheader = "From: $email \r\n";
-			mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+			mail("john@johnmaguiredeveloper.com", $subject, $formcontent, $mailheader) or die("Error!");
 			echo "Thank You!";
 			?>
 
