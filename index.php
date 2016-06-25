3355f63b81759eeafbe52f6a44678f97940bbd69
John Maguire <jmaguire5588@gmail.com>
2016-06-25 11:10:51 -0400

    update email in contact form

--- index.php
+++ index.php
@@ -80,11 +80,12 @@
 			<p>Message</p><textarea name="message" rows="6" cols="25"></textarea><br />
 			<input type="submit" value="Send"><input type="reset" value="Clear">
 		</form>
-			<?php $name = $_POST['name'];
+			<?php 
+			$name = $_POST['name'];
 			$email = $_POST['email'];
 			$message = $_POST['message'];
 			$formcontent="From: $name \n Message: $message";
-			$recipient = "emailaddress@here.com";
+			$recipient = "john@johnmaguiredeveloper.com";
 			$subject = "Contact Form";
 			$mailheader = "From: $email \r\n";
 			mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
